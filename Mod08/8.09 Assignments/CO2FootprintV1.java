25235e54df15309d5b37e47cc82d43f3480535e6
Glen Halonen <glenhalonen@gmail.com>
2016-12-24 14:59:48 -0500

    Module 8

--- Mod08/8.09 Assignments/CO2FootprintV1.java
+++ Mod08/8.09 Assignments/CO2FootprintV1.java
@@ -7,27 +7,5 @@
  */
 public class CO2FootprintV1
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class CO2FootprintV1
-     */
-    public CO2FootprintV1()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
+    
 }
